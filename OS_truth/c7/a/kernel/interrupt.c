2ea41eead8441939f4e32f12b18f2f38a6d35e87
RandyLambert <1249604462@qq.com>
2020-11-09 23:20:07 +0800

    update OS_truth c7

--- OS_truth/c7/a/kernel/interrupt.c
+++ OS_truth/c7/a/kernel/interrupt.c
@@ -3,7 +3,7 @@
 #include "global.h"
 #include "io.h"
 #include "print.h"
-
+//4个宏分别表示主从片的控制端口和数据端口
 #define PIC_M_CTRL 0x20	       // 这里用的可编程中断控制器是8259A,主片的控制端口是0x20
 #define PIC_M_DATA 0x21	       // 主片的数据端口是0x21
 #define PIC_S_CTRL 0xa0	       // 从片的控制端口是0xa0
@@ -27,8 +27,8 @@ static struct gate_desc idt[IDT_DESC_CNT];   // idt是中断描述符表,本质�
 extern intr_handler intr_entry_table[IDT_DESC_CNT];	    // 声明引用定义在kernel.S中的中断处理函数入口数组
 
 /* 初始化可编程中断控制器8259A */
-static void pic_init(void) {
-
+static void pic_init(void) {    //对于8259a的设置是用pic_init函数完成的,他在idt_init中被调用
+                            //pic_init中一次设置主片和从片,无论是珠片还是从片都得按顺序一次写入icw1,icw2,icw3,icw4
    /* 初始化主片 */
    outb (PIC_M_CTRL, 0x11);   // ICW1: 边沿触发,级联8259, 需要ICW4.
    outb (PIC_M_DATA, 0x20);   // ICW2: 起始中断向量号为0x20,也就是IR[0-7] 为 0x20 ~ 0x27.
@@ -36,14 +36,14 @@ static void pic_init(void) {
    outb (PIC_M_DATA, 0x01);   // ICW4: 8086模式, 正常EOI
 
    /* 初始化从片 */
-   outb (PIC_S_CTRL, 0x11);	// ICW1: 边沿触发,级联8259, 需要ICW4.
-   outb (PIC_S_DATA, 0x28);	// ICW2: 起始中断向量号为0x28,也就是IR[8-15] 为 0x28 ~ 0x2F.
-   outb (PIC_S_DATA, 0x02);	// ICW3: 设置从片连接到主片的IR2引脚
-   outb (PIC_S_DATA, 0x01);	// ICW4: 8086模式, 正常EOI
+   outb (PIC_S_CTRL, 0x11);	// ICW1: 边沿触发,级联8259, 需要ICW4.想从片中发送icw1,其意义和主片的icw1一致,只不过是往从片的偶数地址端口0xa0发送,这是从片的控制端口
+   outb (PIC_S_DATA, 0x28);	// ICW2: 起始中断向量号为0x28,也就是IR[8-15] 为 0x28 ~ 0x2F,设置从片的起始中断向量号
+   outb (PIC_S_DATA, 0x02);	// ICW3: 设置从片连接到主片的IR2引脚,icw3专门设立引脚,这里设置从片链接到主片的那个irq引脚上
+   outb (PIC_S_DATA, 0x01);	// ICW4: 8086模式, 正常EOI,向从片中发送icw4,通主片的icw4一样
 
    /* 打开主片上IR0,也就是目前只接受时钟产生的中断 */
-   outb (PIC_M_DATA, 0xfe);
-   outb (PIC_S_DATA, 0xff);
+   outb (PIC_M_DATA, 0xfe);//开始设置imr寄存器只放行时钟中断
+   outb (PIC_S_DATA, 0xff);//只放行时钟中断
 
    put_str("   pic_init done\n");
 }
@@ -57,24 +57,24 @@ static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler 
    p_gdesc->func_offset_high_word = ((uint32_t)function & 0xFFFF0000) >> 16;
 }
 
-/*初始化中断描述符表*/
+/*初始化中断描述符表,填充中断描述符表*/
 static void idt_desc_init(void) {
    int i;
-   for (i = 0; i < IDT_DESC_CNT; i++) {
-      make_idt_desc(&idt[i], IDT_DESC_ATTR_DPL0, intr_entry_table[i]); 
+   for (i = 0; i < IDT_DESC_CNT; i++) {//通过make_idt_desc函数在中断描述符表中创建了idt_desc_cnt个中断描述符
+      make_idt_desc(&idt[i], IDT_DESC_ATTR_DPL0, intr_entry_table[i]);  //三个采纳数,终端们描述符的指针,终端描述符内的属性和中断描述符内对应的中断处理函数
    }
    put_str("   idt_desc_init done\n");
 }
 
 /*完成有关中断的所有初始化工作*/
-void idt_init() {
+void idt_init() {       //是中断初始化工作的主函数
    put_str("idt_init start\n");
    idt_desc_init();	   // 初始化中断描述符表
    pic_init();		   // 初始化8259A
 
    /* 加载idt */
    uint64_t idt_operand = ((sizeof(idt) - 1) | ((uint64_t)(uint32_t)idt << 16));
-   asm volatile("lidt %0" : : "m" (idt_operand));
+   asm volatile("lidt %0" : : "m" (idt_operand)); //通过内联汇编的形式将变量约束m的形式传给lidt作为操作数
    put_str("idt_init done\n");
 }
 
